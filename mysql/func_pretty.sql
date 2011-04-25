74f8e7768c66d0ae55692d8457a6e5d7cae8a583
Hyunwoo Park <ez.amiryo@gmail.com>
2011-04-25 21:09:51 +0900

    Add some comments

--- mysql/func_pretty.sql
+++ mysql/func_pretty.sql
@@ -1,14 +1,16 @@
 DROP FUNCTION IF EXISTS pretty_size;
 DELIMITER $$
 
-CREATE FUNCTION pretty_size( size DOUBLE)
+CREATE FUNCTION pretty_size( size DOUBLE )
 	RETURNS VARCHAR(255) DETERMINISTIC
 BEGIN
 	DECLARE c INT DEFAULT 0;
 	DECLARE unit INT DEFAULT 1000;
-	DECLARE unitChar CHAR(7) DEFAULT 'KMGTPEY';
+	DECLARE unitChar CHAR(8) DEFAULT 'KMGTPEZY';
 	DECLARE binaryPrefix BOOLEAN DEFAULT 1;
 
+	/* Set binaryPrefix = 1 to use binary unit & prefix */
+	/* See IEC 60027-2 A.2 and ISO/IEC 80000 */
 	IF binaryPrefix = 1 THEN
 		SET unit = 1024;
 	END IF;
@@ -18,14 +20,16 @@ BEGIN
 		SET c = c + 1;
 	END WHILE;
 
+	/* Under 1K, just add 'Byte(s)' */
 	IF c = 0 THEN
 		RETURN CONCAT( size, ' B' );
 	END IF;
 
+	/* Modify as your taste */
 	RETURN CONCAT(
-		FORMAT(size,2),
+		FORMAT( size, 2 ),
 		' ',
-		TRIM(SUBSTR(unitChar,c,1)),
+		SUBSTR( unitChar, c, 1 ),
 		IF( binaryPrefix, 'iB', 'B' )
 		);
 END $$
